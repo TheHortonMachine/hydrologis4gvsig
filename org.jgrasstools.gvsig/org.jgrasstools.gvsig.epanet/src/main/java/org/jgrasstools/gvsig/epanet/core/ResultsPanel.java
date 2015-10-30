418e3a3cd980943e7a8597c9edd9f699379e32bc
Andrea Antonello <andrea.antonello@gmail.com>
2015-10-30 22:52:47 +0100

    working on results and charts

--- org.jgrasstools.gvsig/org.jgrasstools.gvsig.epanet/src/main/java/org/jgrasstools/gvsig/epanet/core/ResultsPanel.java
+++ org.jgrasstools.gvsig/org.jgrasstools.gvsig.epanet/src/main/java/org/jgrasstools/gvsig/epanet/core/ResultsPanel.java
@@ -23,20 +23,18 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.io.File;
 import java.sql.SQLException;
 import java.util.List;
 
 import javax.swing.BorderFactory;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
-import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.SwingUtilities;
 
 import org.gvsig.app.ApplicationLocator;
 import org.gvsig.app.ApplicationManager;
@@ -47,9 +45,19 @@ import org.gvsig.tools.i18n.I18nManager;
 import org.gvsig.tools.swing.api.Component;
 import org.gvsig.tools.swing.api.ToolsSwingLocator;
 import org.gvsig.tools.swing.api.threadsafedialogs.ThreadSafeDialogsManager;
-import org.jgrasstools.gears.io.geopaparazzi.geopap4.TimeUtilities;
 import org.jgrasstools.gears.libs.modules.JGTConstants;
 import org.jgrasstools.gvsig.epanet.database.EpanetRun;
+import org.jgrasstools.gvsig.epanet.database.JunctionsResultsTable;
+import org.jgrasstools.gvsig.epanet.database.JunctionsTable;
+import org.jgrasstools.gvsig.epanet.database.PipesTable;
+import org.jgrasstools.gvsig.epanet.database.PumpsTable;
+import org.jgrasstools.gvsig.epanet.database.ReservoirsTable;
+import org.jgrasstools.gvsig.epanet.database.TanksTable;
+import org.jgrasstools.gvsig.epanet.database.ValvesTable;
+import org.jgrasstools.hortonmachine.modules.networktools.epanet.core.ResultsLinkParameters;
+import org.jgrasstools.hortonmachine.modules.networktools.epanet.core.ResultsNodeParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
@@ -63,6 +71,9 @@ import com.j256.ormlite.support.ConnectionSource;
  */
 public class ResultsPanel extends JPanel implements Component {
     private static final long serialVersionUID = 1L;
+
+    private static final Logger logger = LoggerFactory.getLogger(ResultsPanel.class);
+
     private ApplicationManager applicationManager;
     private PreferencesNode preferences;
     private ThreadSafeDialogsManager dialogManager;
@@ -71,6 +82,17 @@ public class ResultsPanel extends JPanel implements Component {
     private File resultsFile;
 
     private ConnectionSource connectionSource = null;
+    private Dao<JunctionsResultsTable, Long> junctionsResultDao;
+    private JComboBox<String> timeCombo;
+
+    private JComboBox<String> nodesPlotCombo;
+
+    private JComboBox<String> linksPlotCombo;
+
+    private EpanetRun currentSelectedRun;
+    private ResultsLinkParameters currentSelectedLinkVar;
+
+    private ResultsNodeParameters currentSelectedNodeVar;
 
     public ResultsPanel( File resultsFile ) {
         this.resultsFile = resultsFile;
@@ -84,7 +106,7 @@ public class ResultsPanel extends JPanel implements Component {
         // Utilities.centerComponentOnScreen(this);
 
         init();
-        setPreferredSize(new Dimension(800, 700));
+        setPreferredSize(new Dimension(800, 300));
 
     }
 
@@ -99,18 +121,11 @@ public class ResultsPanel extends JPanel implements Component {
 
     private void init() {
         setLayout(new GridBagLayout());
-        int col = 0;
-        int width = 1;
-        int height = 1;
         Insets insets = new Insets(5, 5, 5, 5);
 
         GridBagConstraints c = new GridBagConstraints();
-        c.gridx = col;
-        c.gridy = 0;
-        c.gridwidth = width;
-        c.gridheight = height;
         c.weightx = 0.0;
-        c.weighty = 1.0;
+        c.weighty = 0.5;
         c.anchor = GridBagConstraints.PAGE_START;
         c.fill = GridBagConstraints.BOTH;
         c.insets = insets;
@@ -120,27 +135,33 @@ public class ResultsPanel extends JPanel implements Component {
         c.gridx = 0;
         c.gridy = 0;
         c.weightx = 1;
+        c.weighty = 1;
+        c.gridheight = 3;
         JPanel runPanel = new JPanel(new GridBagLayout());
         runPanel.setBorder(BorderFactory.createTitledBorder("Epanet Runs"));
         add(runPanel, c);
 
         c.gridx = 1;
         c.gridy = 0;
-        c.weightx = 2;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridheight = 1;
         JPanel variablesPanel = new JPanel(new GridBagLayout());
         variablesPanel.setBorder(BorderFactory.createTitledBorder("Variables"));
         add(variablesPanel, c);
 
-        c.gridx = 0;
+        c.gridx = 1;
         c.gridy = 1;
         c.weightx = 1;
+        c.weighty = 1;
         JPanel timeLinePanel = new JPanel(new GridBagLayout());
         timeLinePanel.setBorder(BorderFactory.createTitledBorder("Timeline"));
         add(timeLinePanel, c);
 
         c.gridx = 1;
-        c.gridy = 1;
+        c.gridy = 2;
         c.weightx = 1;
+        c.weighty = 1;
         JPanel plotPanel = new JPanel(new GridBagLayout());
         plotPanel.setBorder(BorderFactory.createTitledBorder("Plots"));
         add(plotPanel, c);
@@ -150,27 +171,26 @@ public class ResultsPanel extends JPanel implements Component {
             connectionSource = new JdbcConnectionSource(databaseUrl);
 
             Dao<EpanetRun, Long> epanetRunDao = DaoManager.createDao(connectionSource, EpanetRun.class);
+            junctionsResultDao = DaoManager.createDao(connectionSource, JunctionsResultsTable.class);
 
-            // Dao<JunctionsTable, Long> junctionsDao = DaoManager.createDao(connectionSource,
-            // JunctionsTable.class);
-            // Dao<PipesTable, Long> pipesDao = DaoManager.createDao(connectionSource,
-            // PipesTable.class);
-            // Dao<PumpsTable, Long> pumpsDao = DaoManager.createDao(connectionSource,
-            // PumpsTable.class);
-            // Dao<ValvesTable, Long> valvesDao = DaoManager.createDao(connectionSource,
-            // ValvesTable.class);
-            // Dao<TanksTable, Long> tanksDao = DaoManager.createDao(connectionSource,
-            // TanksTable.class);
-            // Dao<ReservoirsTable, Long> reservoirsDao = DaoManager.createDao(connectionSource,
-            // ReservoirsTable.class);
+            final Dao<JunctionsTable, Long> junctionsDao = DaoManager.createDao(connectionSource, JunctionsTable.class);
+            final Dao<PipesTable, Long> pipesDao = DaoManager.createDao(connectionSource, PipesTable.class);
+            final Dao<PumpsTable, Long> pumpsDao = DaoManager.createDao(connectionSource, PumpsTable.class);
+            final Dao<ValvesTable, Long> valvesDao = DaoManager.createDao(connectionSource, ValvesTable.class);
+            final Dao<TanksTable, Long> tanksDao = DaoManager.createDao(connectionSource, TanksTable.class);
+            final Dao<ReservoirsTable, Long> reservoirsDao = DaoManager.createDao(connectionSource, ReservoirsTable.class);
+
+            /*
+             * RUNPANEL
+             */
 
             List<EpanetRun> allRuns = epanetRunDao.queryForAll();
-            
-            EpanetRun[] runs = new EpanetRun[allRuns.size()+1];
+
+            EpanetRun[] runs = new EpanetRun[allRuns.size() + 1];
             runs[0] = null;
             int i = 1;
             for( EpanetRun epanetRun : allRuns ) {
-                runs[i] = epanetRun;
+                runs[i++] = epanetRun;
             }
 
             final JComboBox<EpanetRun> runsCombo = new JComboBox<EpanetRun>(runs);
@@ -188,19 +208,184 @@ public class ResultsPanel extends JPanel implements Component {
             runPanel.add(descriptionArea, c);
 
             runsCombo.addActionListener(new ActionListener(){
-                public void actionPerformed( ActionEvent e ) {
-                    EpanetRun selectedRun = (EpanetRun) runsCombo.getSelectedItem();
 
+                public void actionPerformed( ActionEvent e ) {
+                    currentSelectedRun = (EpanetRun) runsCombo.getSelectedItem();
                     StringBuilder sb = new StringBuilder();
-                    if (selectedRun != null) {
-                        sb.append("Title: ").append(selectedRun.getTitle()).append("\n");
-                        sb.append("Description: ").append(selectedRun.getDescription()).append("\n");
-                        sb.append("User: ").append(selectedRun.getUser()).append("\n");
+                    if (currentSelectedRun != null) {
+                        sb.append("Title: ").append(currentSelectedRun.getTitle()).append("\n");
+                        sb.append("Description: ").append(currentSelectedRun.getDescription()).append("\n");
+                        sb.append("User: ").append(currentSelectedRun.getUser()).append("\n");
                         sb.append("Timestamp: ")
-                                .append(selectedRun.getUtcTime().toString(JGTConstants.dateTimeFormatterYYYYMMDDHHMMSS))
+                                .append(currentSelectedRun.getUtcTime().toString(JGTConstants.dateTimeFormatterYYYYMMDDHHMMSS))
                                 .append("\n");
                     }
                     descriptionArea.setText(sb.toString());
+
+                    try {
+                        String[] timesList = EpanetUtilities.getTimesList(junctionsResultDao, currentSelectedRun);
+                        timeCombo.setModel(new DefaultComboBoxModel<String>(timesList));
+                        if (timesList.length == 0) {
+                            // run has no data
+                            dialogManager.messageDialog(
+                                    "The run has no data stored. The simulation probably stopped before being able to produce any data.",
+                                    "WARNING", JOptionPane.WARNING_MESSAGE);
+                        }
+
+                        String[] nodes = EpanetUtilities.getNodes(junctionsDao, tanksDao, reservoirsDao, currentSelectedRun);
+                        nodesPlotCombo.setModel(new DefaultComboBoxModel<String>(nodes));
+                        String[] links = EpanetUtilities.getLinks(pipesDao, pumpsDao, valvesDao, currentSelectedRun);
+                        linksPlotCombo.setModel(new DefaultComboBoxModel<String>(links));
+
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            });
+
+            /*
+             * VARIABLES PANEL
+             */
+            JLabel nodesLabel = new JLabel("Node variable");
+            c.gridx = 0;
+            c.gridy = 0;
+            c.weightx = 1;
+            c.weighty = 0;
+            variablesPanel.add(nodesLabel, c);
+
+            ResultsNodeParameters[] nodeVariables = ResultsNodeParameters.values();
+            String[] nodeVariablesStrings = new String[nodeVariables.length];
+            for( int j = 0; j < nodeVariablesStrings.length; j++ ) {
+                nodeVariablesStrings[j] = nodeVariables[j].getKey();
+            }
+            currentSelectedNodeVar = nodeVariables[0];
+
+            final JComboBox<String> nodesCombo = new JComboBox<String>(nodeVariablesStrings);
+            c.gridx = 1;
+            c.gridy = 0;
+            c.weightx = 1;
+            c.weighty = 0;
+            variablesPanel.add(nodesCombo, c);
+            nodesCombo.addActionListener(new ActionListener(){
+                public void actionPerformed( ActionEvent e ) {
+                    Object selectedItem = nodesCombo.getSelectedItem();
+                    if (selectedItem instanceof String) {
+                        String selectedNodeId = (String) selectedItem;
+                        currentSelectedNodeVar = ResultsNodeParameters.forCode(selectedNodeId);
+                    }
+                }
+            });
+
+            JLabel linksLabel = new JLabel("Link variable");
+            c.gridx = 0;
+            c.gridy = 1;
+            c.weightx = 1;
+            c.weighty = 0;
+            variablesPanel.add(linksLabel, c);
+
+            ResultsLinkParameters[] linkVariables = ResultsLinkParameters.values();
+            String[] linkVariablesStrings = new String[linkVariables.length];
+            for( int j = 0; j < linkVariablesStrings.length; j++ ) {
+                linkVariablesStrings[j] = linkVariables[j].getKey();
+            }
+            currentSelectedLinkVar = linkVariables[0];
+
+            final JComboBox<String> linksCombo = new JComboBox<String>(linkVariablesStrings);
+            c.gridx = 1;
+            c.gridy = 1;
+            c.weightx = 1;
+            c.weighty = 0;
+            variablesPanel.add(linksCombo, c);
+            linksCombo.addActionListener(new ActionListener(){
+                public void actionPerformed( ActionEvent e ) {
+                    Object selectedItem = linksCombo.getSelectedItem();
+                    if (selectedItem instanceof String) {
+                        String selectedLinkId = (String) selectedItem;
+                        currentSelectedLinkVar = ResultsLinkParameters.forCode(selectedLinkId);
+                    }
+                }
+            });
+
+            /*
+             * TIMELINE PANEL
+             */
+            JLabel timeLabel = new JLabel("Select time");
+            c.gridx = 0;
+            c.gridy = 0;
+            c.weightx = 1;
+            c.weighty = 0;
+            timeLinePanel.add(timeLabel, c);
+
+            timeCombo = new JComboBox<String>(new String[]{null});
+            c.gridx = 1;
+            c.gridy = 0;
+            c.weightx = 1;
+            c.weighty = 0;
+            timeLinePanel.add(timeCombo, c);
+            timeCombo.addActionListener(new ActionListener(){
+                public void actionPerformed( ActionEvent e ) {
+                    String selectedTime = (String) timeCombo.getSelectedItem();
+                    logger.info("Selected time: " + selectedTime);
+
+                    // DateTime currentSelectedTime =
+                    // OmsEpanet.formatter.parseDateTime(selectedTime);
+
+                    // TODO color the layers according to results
+                }
+            });
+
+            /*
+             * PLOT PANEL
+             */
+            JLabel nodesPlotLabel = new JLabel("Plot Node");
+            c.gridx = 0;
+            c.gridy = 0;
+            c.weightx = 1;
+            c.weighty = 0;
+            plotPanel.add(nodesPlotLabel, c);
+
+            nodesPlotCombo = new JComboBox<String>(new String[]{null});
+            c.gridx = 1;
+            c.gridy = 0;
+            c.weightx = 1;
+            c.weighty = 0;
+            plotPanel.add(nodesPlotCombo, c);
+            nodesPlotCombo.addActionListener(new ActionListener(){
+                public void actionPerformed( ActionEvent e ) {
+                    String selectedNode = (String) nodesPlotCombo.getSelectedItem();
+                    logger.info("Selected Node: " + selectedNode);
+                    if (currentSelectedRun != null)
+                        try {
+                            EpanetUtilities.chartNode(connectionSource, currentSelectedRun, selectedNode, currentSelectedNodeVar);
+                        } catch (Exception e1) {
+                            e1.printStackTrace();
+                        }
+                }
+            });
+
+            JLabel linksPlotLabel = new JLabel("Plot Link");
+            c.gridx = 0;
+            c.gridy = 1;
+            c.weightx = 1;
+            c.weighty = 0;
+            plotPanel.add(linksPlotLabel, c);
+
+            linksPlotCombo = new JComboBox<String>(new String[]{null});
+            c.gridx = 1;
+            c.gridy = 1;
+            c.weightx = 1;
+            c.weighty = 0;
+            plotPanel.add(linksPlotCombo, c);
+            linksPlotCombo.addActionListener(new ActionListener(){
+                public void actionPerformed( ActionEvent e ) {
+                    String selectedLink = (String) linksPlotCombo.getSelectedItem();
+                    logger.info("Selected Link: " + selectedLink);
+                    if (currentSelectedRun != null)
+                        try {
+                            EpanetUtilities.chartLink(connectionSource, currentSelectedRun, selectedLink, currentSelectedLinkVar);
+                        } catch (Exception e1) {
+                            e1.printStackTrace();
+                        }
                 }
             });
 
