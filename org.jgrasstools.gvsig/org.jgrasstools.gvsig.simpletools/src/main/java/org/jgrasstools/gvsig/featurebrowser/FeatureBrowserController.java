a76d969cbe75582bc4a45c27a308402e864e5827
Andrea Antonello <andrea.antonello@gmail.com>
2017-07-28 15:28:55 +0200

    add empty layer and fixes

--- org.jgrasstools.gvsig/org.jgrasstools.gvsig.simpletools/src/main/java/org/jgrasstools/gvsig/featurebrowser/FeatureBrowserController.java
+++ org.jgrasstools.gvsig/org.jgrasstools.gvsig.simpletools/src/main/java/org/jgrasstools/gvsig/featurebrowser/FeatureBrowserController.java
@@ -9,7 +9,8 @@ import java.util.List;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComponent;
 
-import org.gvsig.fmap.dal.exception.DataException;
+import org.cresques.cts.ICoordTrans;
+import org.cresques.cts.IProjection;
 import org.gvsig.fmap.dal.feature.Feature;
 import org.gvsig.fmap.dal.feature.FeatureSelection;
 import org.gvsig.fmap.geom.Geometry;
@@ -17,17 +18,17 @@ import org.gvsig.fmap.geom.primitive.Envelope;
 import org.gvsig.fmap.mapcontext.MapContext;
 import org.gvsig.fmap.mapcontext.layers.vectorial.FLyrVect;
 import org.gvsig.tools.swing.api.Component;
-import org.gvsig.tools.swing.api.ToolsSwingLocator;
-import org.gvsig.tools.swing.api.threadsafedialogs.ThreadSafeDialogsManager;
 import org.jgrasstools.gvsig.base.FeatureUtilities;
 import org.jgrasstools.gvsig.base.JGTUtilities;
 import org.jgrasstools.gvsig.base.LayerUtilities;
+import org.jgrasstools.gvsig.base.ProjectUtilities;
 
 public class FeatureBrowserController extends FeatureBrowserView implements Component {
 
+    private static final long serialVersionUID = 1L;
+    private static final String EMPTY_LAYER = " - ";
     private HashMap<String, FLyrVect> name2LayersMap = new HashMap<String, FLyrVect>();
     private MapContext currentMapcontext;
-    private ThreadSafeDialogsManager dialogManager = ToolsSwingLocator.getThreadSafeDialogsManager();
 
     private String selectedLayerName;
 
@@ -43,19 +44,20 @@ public class FeatureBrowserController extends FeatureBrowserView implements Comp
     }
 
     private void init() {
-        
+
         // TODO implement buffer part
         _bufferLabel.setVisible(false);
         _bufferText.setVisible(false);
         // end TODO
-        
+
         setCombos();
 
         _layerCombo.addActionListener(new ActionListener(){
-
-
             public void actionPerformed( ActionEvent e ) {
                 selectedLayerName = (String) _layerCombo.getSelectedItem();
+                if (selectedLayerName.equals(EMPTY_LAYER)) {
+                    return;
+                }
 
                 selectedLayer = name2LayersMap.get(selectedLayerName);
                 if (selectedLayer != null) {
@@ -76,10 +78,9 @@ public class FeatureBrowserController extends FeatureBrowserView implements Comp
                 }
             }
         });
-        
-        
+
         _nextButton.addActionListener(new ActionListener(){
-            
+
             @Override
             public void actionPerformed( ActionEvent e ) {
                 featureCount++;
@@ -90,7 +91,7 @@ public class FeatureBrowserController extends FeatureBrowserView implements Comp
             }
         });
         _previousButton.addActionListener(new ActionListener(){
-            
+
             @Override
             public void actionPerformed( ActionEvent e ) {
                 featureCount--;
@@ -112,40 +113,58 @@ public class FeatureBrowserController extends FeatureBrowserView implements Comp
             sb.append(attrName).append("=").append(feature.get(attrName).toString()).append("\n");
         }
         _infoTextArea.setText(sb.toString());
-        
+
         Geometry defaultGeometry = feature.getDefaultGeometry();
+
         Envelope envelope = defaultGeometry.getEnvelope();
-        
-//        String bufferStr = _bufferText.getText();
-//        double buffer = 0;
-//        try {
-//            buffer = Double.parseDouble(bufferStr);
-//        } catch (NumberFormatException e) {
-//            // ignore
-//        }
-        JGTUtilities.zoomTo(envelope);
-        
+        double length = envelope.getLength(0);
+        length = Math.max(length, envelope.getLength(1));
+        double buffer = length * 0.9;
         try {
+            Geometry bufferGeom = defaultGeometry.buffer(buffer);
+            envelope = bufferGeom.getEnvelope();
+
+            IProjection dataCrs = feature.getDefaultSRS();
+
+            // reproject to map's crs
+            IProjection mapCrs = currentMapcontext.getViewPort().getProjection();
+            ICoordTrans coordTrans = dataCrs.getCT(mapCrs);
+            Geometry envelopeGeometry = envelope.getGeometry();
+
+            envelopeGeometry.reProject(coordTrans);
+            Envelope reprojectedEnvelope = envelopeGeometry.getEnvelope();
+
+            // String bufferStr = _bufferText.getText();
+            // double buffer = 0;
+            // try {
+            // buffer = Double.parseDouble(bufferStr);
+            // } catch (NumberFormatException e) {
+            // // ignore
+            // }
+            JGTUtilities.zoomTo(reprojectedEnvelope);
+
             FeatureSelection featureSelection = selectedLayer.getFeatureStore().getFeatureSelection();
             featureSelection.deselectAll();
             featureSelection.select(feature);
-        } catch (DataException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    @SuppressWarnings("unchecked")
     private void setCombos() {
         try {
+            currentMapcontext = ProjectUtilities.getCurrentMapcontext();
             List<FLyrVect> vectorLayers = LayerUtilities.getVectorLayers(currentMapcontext);
-            
+
             if (vectorLayers.size() == name2LayersMap.size()) {
                 return;
             }
-            
-            
+
             name2LayersMap = new HashMap<String, FLyrVect>();
-            String[] names = new String[vectorLayers.size()];
-            int count = 0;
+            String[] names = new String[vectorLayers.size() + 1];
+            names[0] = EMPTY_LAYER;
+            int count = 1;
             for( FLyrVect fLyrVect : vectorLayers ) {
                 String name = fLyrVect.getName();
                 name2LayersMap.put(name, fLyrVect);
