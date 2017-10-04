35fc3c6016af0d055c330488ea7ac2a83c52a364
Andrea Antonello <andrea.antonello@gmail.com>
2017-10-04 13:29:00 +0200

    remove until nww is not properly referenced

--- org.hortonmachine.gvsig/org.hortonmachine.gvsig.geopaparazzi/src/main/java/org/hortonmachine/gvsig/geopaparazzi/GvsigGeopaparazziViewer.java
+++ org.hortonmachine.gvsig/org.hortonmachine.gvsig.geopaparazzi/src/main/java/org/hortonmachine/gvsig/geopaparazzi/GvsigGeopaparazziViewer.java
@@ -33,7 +33,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The spatialtoolbox view controller.
+ * The geopaparazzi view controller.
  * 
  * @author Andrea Antonello (www.hydrologis.com)
  *
